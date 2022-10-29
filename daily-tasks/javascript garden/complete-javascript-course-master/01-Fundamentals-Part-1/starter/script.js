7eadd54804c9c5ceb1ea14483752d58b31439269
Debanjan Be <debanjanb01234@gmail.com>
2022-10-29 18:52:06 +0530

    data types in js

---
+++ daily-tasks/javascript garden/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,8 @@
+// alert(Infinity);
+// alert("not a number" / 2);
+// alert(NaN * 0);
+// alert(NaN ** 0 + 2);
+// alert(typeof (12 + true)); // number
+// alert(typeof 12 + true); // numbertrue
+// alert(typeof typeof x);
+// alert(typeof (100 ** 4000));
