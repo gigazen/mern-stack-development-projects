a7ce55093f8087ba277b26909be50c643b8e1a39
Debanjan Be <debanjanb01234@gmail.com>
2022-10-30 19:15:02 +0530

    const let var keywords

--- daily-tasks/javascript garden/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ daily-tasks/javascript garden/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,8 +1,6 @@
-// alert(Infinity);
-// alert("not a number" / 2);
-// alert(NaN * 0);
-// alert(NaN ** 0 + 2);
-// alert(typeof (12 + true)); // number
-// alert(typeof 12 + true); // numbertrue
-// alert(typeof typeof x);
-// alert(typeof (100 ** 4000));
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+const job;
