0024795a8b23308369b450aff764d53815bab948
Debanjan Be <debanjanb01234@gmail.com>
2022-11-02 19:35:13 +0530

    let is block scope variable

--- daily-tasks/javascript garden/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ daily-tasks/javascript garden/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,10 @@
-let age = 30;
-age = 31;
-
-const birthYear = 1989;
-// birthYear = 1990;
+if (true) {
+  // Declare name block variable
+  let month;
+  console.log(month); // => undefined
+  // Declare and initialize year block variable
+  let year = 1994;
+  console.log(year); // => 1994
+}
+// name and year or not accessible here, outside the block
+console.log(year); // ReferenceError: year is not defined
