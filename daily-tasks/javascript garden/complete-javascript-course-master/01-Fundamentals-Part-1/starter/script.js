9e85b638a1535263e70f6c98556f2629c2f4142c
Debanjan Be <debanjanb01234@gmail.com>
2022-11-07 19:27:19 +0530

    different data types in js

--- daily-tasks/javascript garden/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ daily-tasks/javascript garden/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,6 +1,19 @@
-let age = 100;
-
-// change the value to undefined
-age = undefined;
-
-alert(age); // "undefined"
+var v;
+typeof v; // 'undefined'
+typeof doesnotExist; // 'undefined'
+v = "1";
+typeof v; // 'string'
+v = 2;
+typeof v; // 'number'
+v = true;
+typeof v; // 'boolean'
+v = {};
+typeof v; // 'object'
+v = Symbol();
+typeof v; // 'symbol'
+var v = null;
+typeof v; // 'object' OOPS, bug!! should be null type
+v = function () {};
+typeof v; // 'function' why?? function is subtype of object, so it should be object, although it is useful
+v = [1, 2, 3];
+typeof v; // 'object'  why?? function is subtype of object, typeof returns the subtype; array is also subtype of object, but typeof returns main type object
