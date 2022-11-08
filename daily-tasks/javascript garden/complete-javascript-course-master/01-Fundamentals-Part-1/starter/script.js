51b001bfc73aa3b1c62b08a507fb8b2c132b0869
Debanjan Be <debanjanb01234@gmail.com>
2022-11-08 19:04:18 +0530

    when types are same, == and === works same

--- daily-tasks/javascript garden/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ daily-tasks/javascript garden/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,19 +1,12 @@
-var v;
-typeof v; // 'undefined'
-typeof doesnotExist; // 'undefined'
-v = "1";
-typeof v; // 'string'
-v = 2;
-typeof v; // 'number'
-v = true;
-typeof v; // 'boolean'
-v = {};
-typeof v; // 'object'
-v = Symbol();
-typeof v; // 'symbol'
-var v = null;
-typeof v; // 'object' OOPS, bug!! should be null type
-v = function () {};
-typeof v; // 'function' why?? function is subtype of object, so it should be object, although it is useful
-v = [1, 2, 3];
-typeof v; // 'object'  why?? function is subtype of object, typeof returns the subtype; array is also subtype of object, but typeof returns main type object
+var studentName1 = "Frank";
+var studentName2 = `${studentName1}`;
+
+var workshopEnrollment1 = 16;
+var workshopEnrollment2 = workshopEnrollment1 + 0;
+
+// when types are same, both == and === works same
+studentName1 == studentName2; // true
+studentName1 === studentName2; // true
+
+workshopEnrollment1 == workshopEnrollment2; // true
+workshopEnrollment1 === workshopEnrollment2; // true
